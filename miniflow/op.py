c37e04d5a4bdbb0bbca054e6fa5fdccc91580e4b
psz <2422158541@qq.com>
2024-08-20 22:13:14 +0800

    add AddOp

--- miniflow/op.py
+++ miniflow/op.py
@@ -8,3 +8,14 @@ class Op:
         raise NotImplementedError
     def gradient(self):
         raise NotImplementedError
+    
+    
+class AddOp(Op):
+    def __call__(self,node_A, node_B):
+        new_node = super().__call__()
+        new_node.inputs = [node_A, node_B]
+        return new_node
+    def compute(self, node, input_vals):
+        return input_vals[0] + input_vals[1]
+    def gradient(self, node, output_grad):
+        return [output_grad,output_grad]
