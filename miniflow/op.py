b857e8d131e3f771dd8cd3545683b9426fb6add5
psz <2422158541@qq.com>
2024-08-20 22:16:33 +0800

    add MulOp

--- miniflow/op.py
+++ miniflow/op.py
@@ -19,3 +19,13 @@ class AddOp(Op):
         return input_vals[0] + input_vals[1]
     def gradient(self, node, output_grad):
         return [output_grad,output_grad]
+
+class MulOp(Op):
+    def __call__(self,node_A, node_B):
+        new_node = super().__call__()
+        new_node.inputs = [node_A, node_B]
+        return new_node
+    def compute(self, node, input_vals):
+        return input_vals[0] * input_vals[1]
+    def gradient(self, node, output_grad):
+        return [output_grad*node.inputs[1],output_grad*node.inputs[0]]
