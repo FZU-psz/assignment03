68dcd1e73b144923ab7ed039442ea7c6aa331799
psz <2422158541@qq.com>
2024-08-30 17:46:13 +0800

    add minist

--- miniflow/data.py
+++ miniflow/data.py
@@ -1,47 +1,77 @@
 import numpy as np
+import os 
+import gzip
+import pickle
 
 class Dataset:
-    def __init__(self, name, data, labels):
-        self.name = name
+    def __init__(self,  data, labels):
+
         self.data = data
         self.labels = labels
-        self.index_in_epoch = 0
-        self.num_examples = data.shape[0]
-        self.epochs_completed = 0
+    
+    def __getitem__(self, index):
+        return self.data[index], self.labels[index]
+    
+    def __len__(self):
+        return len(self.data)
 
-    def next_batch(self, batch_size):
-        start = self.index_in_epoch
-        self.index_in_epoch += batch_size
-
-        if self.index_in_epoch > self.num_examples:
-            self.epochs_completed += 1
-            perm = np.arange(self.num_examples)
-            np.random.shuffle(perm)
-            self.data = self.data[perm]
-            self.labels = self.labels[perm]
-            start = 0
-            self.index_in_epoch = batch_size
-            assert batch_size <= self.num_examples
+class DataLoader:
+    def __init__(self, dataset:Dataset, batch_size:int):
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.current_index = 0
+    
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.current_index >= len(self.dataset):
+            self.current_index = 0
         
-        end = self.index_in_epoch
-        return self.data[start:end], self.labels[start:end]
+        end = min(self.current_index+self.batch_size, len(self.dataset))
+        batch_data = self.dataset.data[self.current_index:end]
+        batch_labels = self.dataset.labels[self.current_index:end]
+        self.current_index += self.batch_size
+        return batch_data, batch_labels
+        
 
-def load_mnist_data(data_path):
-    import os
-    import gzip
-    import numpy as np
+def load_mnist_data(dataset):
+    """ Load the dataset
+    Code adapted from http://deeplearning.net/tutorial/code/logistic_sgd.py
 
-    def load_data(filename, label=False):
-        with gzip.open(filename) as gz:
-            data = np.frombuffer(gz.read(), dtype=np.uint8)
-            data = data[16:].reshape(-1, 28*28).astype(np.float32)
-        return data
+    :type dataset: string
+    :param dataset: the path to the dataset (here MNIST)
+    """
+    # Download the MNIST dataset if it is not present
+    data_dir, data_file = os.path.split(dataset)
+    if data_dir == "" and not os.path.isfile(dataset):
+        # Check if dataset is in the data directory.
+        new_path = os.path.join(
+            os.path.split(__file__)[0],
+            dataset
+        )
+        if os.path.isfile(new_path) or data_file == 'mnist.pkl.gz':
+            dataset = new_path
 
-    def load_labels(filename):
-        with gzip.open(filename) as gz:
-            labels = np.frombuffer(gz.read(), dtype=np.uint8, offset=8)
-        return labels
+    if (not os.path.isfile(dataset)) and data_file == 'mnist.pkl.gz':
+        from six.moves import urllib
+        origin = (
+            'http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz'
+        )
+        print('Downloading data from %s' % origin)
+        urllib.request.urlretrieve(origin, dataset)
 
-    data = load_data(os.path.join(data_path, 'train-images-idx3-ubyte.gz'))
-    labels = load_labels(os.path.join(data_path, 'train-labels-idx1-ubyte.gz'))
-    return Dataset('train', data, labels)
+    print('Loading data...')
+
+    # Load the dataset
+    with gzip.open(dataset, 'rb') as f:
+        try:
+            train_set, valid_set, test_set = pickle.load(f, encoding='latin1')
+        except:
+            train_set, valid_set, test_set = pickle.load(f)
+    # train_set, valid_set, test_set format: tuple(input, target)
+    # input is a numpy.ndarray of 2 dimensions (a matrix), np.float32
+    # where each row corresponds to an example. target is a
+    # numpy.ndarray of 1 dimension (vector), np.int64 that has the same length
+    # as the number of rows in the input. It should give the target
+    # to the example with the same index in the input.
+    return train_set, valid_set, test_set
